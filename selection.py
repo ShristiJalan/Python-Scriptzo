6dddbb391cae53d3685caaa77c6e445f9dffe17d
ShristiJalan <shristijalan1010@gmail.com>
2021-03-01 17:45:09 +0530

    selection program

---
+++ selection.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter num1: "))
+if(num1 > 10 and num1 < 20):
+    print("num1 is in between 10 and 20")
+elif(num1 > 20 and num1 < 30):
+    print("num1 is in between 20 and 30")
+else:
+    print("num1 is a number")
+    
+        
